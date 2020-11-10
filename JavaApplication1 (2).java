a76fcd5be06723d36621f9b64f0a92cdd1ce53e5
StefPiturca <74204702+StefPiturca@users.noreply.github.com>
2020-11-10 17:54:13 +0200

    Add files via upload

---
+++ JavaApplication1 (2).java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author vic
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     String charactername = "Dave";
+     int characterage = 20;
+     System.out.println("Hello World");   // TODO code application logic here
+    System.out.println("There once was a man named " + charactername );
+    System.out.println("he was "+ characterage + " years old");
+    System.out.println("He really liked the name " + charactername + " but didn't like being " + characterage);
+    System.out.println("Most of his friends were als0" + charactername +"and one of them was also named " + charactername);
+    }
+    
+    
+}
