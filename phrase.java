5ff42b505ad623f7ee6848c0e267695ebf3f3c32
StefPiturca <74204702+StefPiturca@users.noreply.github.com>
2020-11-12 07:35:05 +0200

    Add files via upload

---
+++ phrase.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vic
+ */
+class phrase {
+ public static void main(String [] args) {   
+   String phrase = "To be or not to be";
+   char grade;
+     grade = 'B';
+   int age = 35;
+   double gpa = 2.7;
+   boolean 
+     male;
+     male = false;
+   System.out.println(age);
+   System.out.println(gpa);
+ 
+ }
+ }
+
