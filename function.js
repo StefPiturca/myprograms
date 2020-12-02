e66f239d3afa9631f67e2e6cb2eaae6838097ac1
StefPiturca <74204702+StefPiturca@users.noreply.github.com>
2020-12-02 14:01:04 +0200

    Create function.js

---
+++ function.js
@@ -0,0 +1,7 @@
+function sayhi(name,age){
+  document.write("<h1>Hello"+ name+ "</h1>");
+  document.write("<p>You are " + age + "</p>")
+}
+sayhi(' Steve', 34);
+sayhi(' Dave',23);
+sayhi('Tom',80);
