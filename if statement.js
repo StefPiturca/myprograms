0d36d03da6d71920d2e1ea6341ec6564d6491aff
StefPiturca <74204702+StefPiturca@users.noreply.github.com>
2020-12-03 11:46:00 +0200

    Add files via upload

---
+++ if statement.js
@@ -0,0 +1,11 @@
+var isMale = True;
+var isTall = True;
+if(isMale & isTall){
+    document.write("You are male");
+} else {
+    document.write("you are not male");
+}
+var newmessage= document.createElement("li");
+newmessage.appendChild("You are male");
+button.addEventListener("");
+newmessage.innerHTML()= newmessage.Value;
